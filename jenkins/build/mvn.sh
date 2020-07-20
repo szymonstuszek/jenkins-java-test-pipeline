6b854098c0936db5bac1cf22e61428794392cdeb
szymonstuszek <szymonstuszek@gmail.com>
2020-07-20 09:32:16 +0000

    Fix workspace path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,6 @@ echo "*************************"
 echo "*******Building jar******"
 echo "*************************"
 
+WORKSPACE=/home/ec2-user/jenkins-data/jenkins_home/workspace/java-test-pipeline
+
 docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,4 +4,6 @@ echo "*************************"
 echo "*******Running tests*****"
 echo "*************************"
 
+WORKSPACE=/home/ec2-user/jenkins-data/jenkins_home/workspace/java-test-pipeline
+
 docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
