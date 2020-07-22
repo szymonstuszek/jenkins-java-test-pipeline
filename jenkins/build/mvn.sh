43a3f07f6a6267acd69864738afa343122a8b939
szymonstuszek <szymonstuszek@gmail.com>
2020-07-22 10:11:26 +0000

    Bugfix

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "*************************"
 echo "*******Building jar******"
 echo "*************************"
 
-WORKSPACE=/home/ec2-user/jenkins-data/jenkins_home/workspace/java-test-pipeline
+WORKSPACE=/home/ec2-user/jenkins-data/jenkins_home/workspace/java-pipeline-test
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
