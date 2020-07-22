54cd5b31f6f69d8280b0a2a5d3b6cafa070147f3
szymonstuszek <szymonstuszek@gmail.com>
2020-07-22 09:48:44 +0000

    Unused script

--- jenkins/deploy/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo maven-project > /tmp/.auth
-echo $BUILD_TAG >> /tmp/.auth
-echo $USERNAME >> /tmp/.auth
-
-scp -i /opt/prod /tmp/.auth  $DEPLOYMENT_USER@$ADDRESS:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish.sh $DEPLOYMENT_USER@$ADDRESS:/tmp/publish
-ssh -i /opt/prod $DEPLOYMENT_USER@$ADDRESS "/tmp/publish"
