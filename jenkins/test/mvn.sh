91eacfe6fb1c61f28a76de39055ea624fb47c1b2
szymonstuszek <szymonstuszek@gmail.com>
2020-07-22 12:36:03 +0000

    Workspace fixed

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,6 +4,5 @@ echo "*************************"
 echo "*******Running tests*****"
 echo "*************************"
 
-WORKSPACE=/home/ec2-user/jenkins-data/jenkins_home/workspace/java-pipeline-test
 
-docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v $CURRENT_WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
