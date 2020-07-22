1e9fd88f08dc0d85d32f1de3f33c2f944c87a3cb
szymonstuszek <szymonstuszek@gmail.com>
2020-07-22 12:20:45 +0000

    Workspace modification

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,8 +4,12 @@ echo "*************************"
 echo "*******Building jar******"
 echo "*************************"
 
-#WORKSPACE=/home/ec2-user/jenkins-data/jenkins_home/workspace/java-pipeline-test
-CURRENT_DIR=$PWD
-echo "current directory: $PWD"
-echo "current directory: $CURRENT_DIR"
-docker run --rm -v $CURRENT_DIR/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+WORKSPACE=/home/ec2-user/jenkins-data/jenkins_home/workspace/java-pipeline-test
+
+echo "$WORKSPACE1"
+echo "$JOB_NAME"
+
+CURRENT_WORKSPACE=$WORKSPACE1$JOB_NAME
+
+echo "current workspace: $CURRENT_WORKSPACE"
+docker run --rm -v $CURRENT_WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
