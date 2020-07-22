899351c28bc77be94b8611db3ef977fa204ee391
szymonstuszek <szymonstuszek@gmail.com>
2020-07-22 11:57:06 +0000

    workspace

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,5 @@ echo "*************************"
 
 WORKSPACE=/home/ec2-user/jenkins-data/jenkins_home/workspace/java-pipeline-test
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+echo "current directory: $PWD"
+docker run --rm -v ./java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
